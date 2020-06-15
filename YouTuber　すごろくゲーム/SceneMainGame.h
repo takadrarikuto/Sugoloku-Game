ab0d4ceb63c48e1cd82a33a31906ffd96fc30f33
高寺 陸斗 <h1201819020@hiratagakuen.onmicrosoft.com>
2020-06-15 16:24:50 +0900

    コミットテスト

---
+++ YouTuber　すごろくゲーム/SceneMainGame.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "GameL\SceneManager.h"
+
+using namespace GameL;
+
+class CSceneMainGame:public:Scene
+{
+public:
+	CSceneMainGame();
+	~CSceneMainGame();
+	void InitScene();
+	void Scene();
+private:
+
+};
